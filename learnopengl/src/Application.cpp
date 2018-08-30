0fbaa78a655ce76cf6859b6f3898b82379a00301
Linas Sabaitis <bufy0u@gmail.com>
2018-08-30 16:42:50 +0300

    branch for Hello Triangle Exercise 1

--- learnopengl/src/Application.cpp
+++ learnopengl/src/Application.cpp
@@ -24,6 +24,8 @@ const char *fragmentShaderSource = "#version 330 core\n"
 	"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
 	"}\n\0";
 
+// HELLO TRIANGLES EXERCISE 1
+
 int main()
 {
 	// glfw: initialize and configure
